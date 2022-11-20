905d160dedd889b3756eb0da870376400085b90b
Vasu Jain <69643310+vasujain2@users.noreply.github.com>
2022-11-20 17:46:31 +0530

    Updated with rfid

--- RoboSafe.ino
+++ RoboSafe.ino
@@ -1,18 +1,57 @@
-// fingerprint sensor
+#include "Arduino.h"
+#include <SoftwareSerial.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <EEPROM.h>
+#include <Wire.h>
 #include <Adafruit_Fingerprint.h>
+
+#define pinRST         9          // Configurable, see typical pin layout above
+#define pinSS          10         // Configurable, see typical pin layout above
+#define lockPin        8
+#define gPin           7
+#define rPin           4
+#define buzzerPin      6
+
+// RFID
+byte readCard[4];
+int successRead;
+String orgkey = "93408390";
+String nkey = "";
+MFRC522 mfrc522(pinSS, pinRST);  // Create MFRC522 instance
+MFRC522::MIFARE_Key key;
+
+
+
+// FingerPrint
 // pin #2 is IN from sensor (GREEN wire)
 // pin #3 is OUT from arduino  (WHITE wire)
 SoftwareSerial mySerial(2, 3);
-
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 
 void setup()  
 {
+  // pinModes
+  pinMode(lockPin, OUTPUT);
+  pinMode(rPin, OUTPUT);
+  pinMode(gPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+  //rfid
+	SPI.begin();			// Init SPI bus
+	mfrc522.PCD_Init();		// Init MFRC522
+	delay(4);				// Optional delay. Some board do need more time after init to be ready, see Readme
+	Serial.println("RFID reading.............");
+  pinMode(lockPin, OUTPUT);
+  do{
+    successRead = getID(mfrc522.uid.uidByte, mfrc522.uid.size);
+  } while (!successRead);
+
+  // FingerPrint
   Serial.begin(9600);
-  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  // while (!Serial);  // For Yun/Leo/Micro/Zero/...
   delay(100);
   Serial.println("\n\nAdafruit finger detect test");
-
   // set the data rate for the sensor serial port
   finger.begin(57600);
   delay(5);
@@ -22,9 +61,7 @@ void setup()
     Serial.println("Did not find fingerprint sensor :(");
     while (1) { delay(1); }
   }
-
   finger.getTemplateCount();
-
     if (finger.templateCount == 0) {
     Serial.print("Sensor doesn't contain any fingerprint data. Please run the 'enroll' example.");
   } 
@@ -34,12 +71,59 @@ void setup()
   }
 }
 
-void loop()                     // run over and over again
+void loop()                     
 {
+  // RFID Loop
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println(" RoboSafe    v1.0"); 
+  display.display();
+  getID(mfrc522.uid.uidByte, mfrc522.uid.size);
+  if (nkey==orgkey){
+    Serial.print("Key Matched!");
+    digitalWrite(lockPin, LOW);
+    digitalWrite(gPin, HIGH);
+    digitalWrite(buzzerPin, HIGH);
+    delay(1000);
+    digitalWrite(buzzerPin, LOW);
+    delay(3000);
+    digitalWrite(gPin, LOW);
+    digitalWrite(lockPin, HIGH);
+    nkey="";
+  }
+
+  // FingerPrint Loop
   getFingerprintIDez();
-  delay(50);            //don't ned to run this at full speed.
+  delay(50);
+
+
 }
 
+
+// RFID Function
+
+int getID(byte *buffer, byte bufferSize) {
+  String read_rfid="";
+  if (!mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  if (!mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+  Serial.println("UID: ");
+    for (byte i = 0; i < bufferSize; i++) {
+        read_rfid=read_rfid + String(buffer[i], HEX);
+    }
+  Serial.println(read_rfid);
+  nkey = read_rfid;
+  return;
+}
+
+
+// FingerPrint Functions
 uint8_t getFingerprintID() {
   uint8_t p = finger.getImage();
   switch (p) {
@@ -105,8 +189,6 @@ uint8_t getFingerprintID() {
 
   return finger.fingerID;
 }
-
-// returns -1 if failed, otherwise returns ID #
 int getFingerprintIDez() {
   uint8_t p = finger.getImage();
   if (p != FINGERPRINT_OK)  return -1;
