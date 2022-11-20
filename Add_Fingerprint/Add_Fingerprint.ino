cf9ae578169e64f4c3c59fe9b54af69e092aead1
rachitgarg2005 <118383312+rachitgarg2005@users.noreply.github.com>
2022-11-20 17:12:08 +0530

    Updated main

--- Add_Fingerprint/Add_Fingerprint.ino
+++ Add_Fingerprint/Add_Fingerprint.ino
@@ -109,7 +109,7 @@ uint8_t getFingerprintID() {
 // returns -1 if failed, otherwise returns ID #
 int getFingerprintIDez() {
   uint8_t p = finger.getImage();
-  if (p != FINGERPRINT_OK)  return -1;
+  if (p != FINGERPRINT_OK)  return -1;    
 
   p = finger.image2Tz();
   if (p != FINGERPRINT_OK)  return -1;
